f3c2e950dd452a40250d75eacb5ec2a5c2a04e6a
Максим Процюк <protsuk26@gmail.com>
2024-12-13 19:30:21 +0200

    Зробив адаптив сторінки і графіки, додав мобільне меню

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,15 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
-    };
-  
+  };
+      const body = document.getElementsByTagName("body");
+
+
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+      body[0].classList.toggle("no-scroll");
     }
   })();
